c3f3190e0a0f0ee50390e042c68fbb60045f0c54
timeloveboy <734991033@qq.com>
2016-12-08 16:00:01 +0800

    ~

--- server.go
+++ server.go
@@ -79,9 +79,9 @@ func (server *Server) allHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	switch r.Method {
 	case http.MethodOptions:
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081") //允许访问所有域
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")         //header的类型
-		w.Header().Set("content-type", "application/json")                     //返回数据格式是json
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081, http://m.xpai.tv") //允许访问所有域
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")                           //header的类型
+		w.Header().Set("content-type", "application/json")                                       //返回数据格式是json
 		return
 	}
 
